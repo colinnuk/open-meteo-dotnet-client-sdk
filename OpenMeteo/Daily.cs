506eac7e3456fb509389bd5fa9e05b2ff269b31a
colinnuk <colinnuk@users.noreply.github.com>
2025-10-01 07:26:54 -0700

    Allow nullable float values in Daily.cs arrays
    Updated the `Daily` class properties to support nullable float
    values within arrays, enabling individual elements to be null.
    
    Added a new (ignored) test method, `MeteoSwissCH1_Null_Daily_Values_Test`,
    to verify behavior when the MeteoSwiss CH1 model returns null
    daily values.

--- OpenMeteo/Daily.cs
+++ OpenMeteo/Daily.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace OpenMeteo
+﻿namespace OpenMeteo
 {
     public class Daily
     {
@@ -12,31 +8,31 @@ namespace OpenMeteo
         /// The most severe daily weather condition
         /// </summary>
         /// <value></value>
-        public float[]? Weathercode { get; set; }
+        public float?[]? Weathercode { get; set; }
 
         /// <summary>
         /// Maximum daily temperature at 2 meters above ground
         /// </summary>
         /// <value></value>
-        public float[]? Temperature_2m_max { get; set; }
+        public float?[]? Temperature_2m_max { get; set; }
 
         /// <summary>
         /// Minimum daily temperature at 2 meters above ground
         /// </summary>
         /// <value></value>
-        public float[]? Temperature_2m_min { get; set; }
+        public float?[]? Temperature_2m_min { get; set; }
 
         /// <summary>
         /// Maximum daily apparent temperature
         /// </summary>
         /// <value></value>
-        public float[]? Apparent_temperature_max { get; set; }
+        public float?[]? Apparent_temperature_max { get; set; }
 
         /// <summary>
         /// Minimum daily apparent temperature
         /// </summary>
         /// <value></value>
-        public float[]? Apparent_temperature_min { get; set; }
+        public float?[]? Apparent_temperature_min { get; set; }
 
         /// <summary>
         /// Sunrise time
@@ -54,60 +50,60 @@ namespace OpenMeteo
         /// Sum of daily precipitation
         /// </summary>
         /// <value></value>
-        public float[]? Precipitation_sum { get; set; }
+        public float?[]? Precipitation_sum { get; set; }
 
         /// <summary>
         /// Sum of daily rain
         /// </summary>
         /// <value></value>
-        public float[]? Rain_sum { get; set; }
+        public float?[]? Rain_sum { get; set; }
 
         /// <summary>
         /// Sum of daily showers
         /// </summary>
         /// <value></value>
-        public float[]? Showers_sum { get; set; }
+        public float?[]? Showers_sum { get; set; }
 
         /// <summary>
         /// Sum of daily snowfall
         /// </summary>
         /// <value></value>
-        public float[]? Snowfall_sum { get; set; }
+        public float?[]? Snowfall_sum { get; set; }
 
         /// <summary>
         /// Daily hours with rain
         /// </summary>
         /// <value></value>
-        public float[]? Precipitation_hours { get; set; }
+        public float?[]? Precipitation_hours { get; set; }
 
         /// <summary>
         /// Maximum daily windspeed at 10 meters above ground
         /// </summary>
         /// <value></value>
-        public float[]? Windspeed_10m_max { get; set; }
+        public float?[]? Windspeed_10m_max { get; set; }
 
         /// <summary>
         /// Maximum daily windgusts at 10 meters above ground
         /// </summary>
         /// <value></value>
-        public float[]? Windgusts_10m_max { get; set; }
+        public float?[]? Windgusts_10m_max { get; set; }
 
         /// <summary>
         /// Dominant daily wind direction at 10 meters above ground
         /// </summary>
         /// <value></value>
-        public float[]? Winddirection_10m_dominant { get; set; }
+        public float?[]? Winddirection_10m_dominant { get; set; }
 
         /// <summary>
         /// Sum of daily solar radiation
         /// </summary>
         /// <value></value>
-        public float[]? Shortwave_radiation_sum { get; set; }
+        public float?[]? Shortwave_radiation_sum { get; set; }
 
         /// <summary>
         /// Sum of daily ET₀ Reference Evapotranspiration of a well watered grass field
         /// </summary>
         /// <value></value>
-        public float[]? Et0_fao_evapotranspiration { get; set; }
+        public float?[]? Et0_fao_evapotranspiration { get; set; }
     }
 }
