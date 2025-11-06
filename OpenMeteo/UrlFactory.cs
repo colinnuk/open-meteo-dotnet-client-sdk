85bfb883e8ec9bdacacdc7b4963d4a8c1b4440ee
colinnuk <colinnuk@users.noreply.github.com>
2025-11-06 08:02:12 -0800

    Start refactoring UrlFactory into a builder pattern in

--- OpenMeteo/UrlFactory.cs
+++ OpenMeteo/UrlFactory.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Globalization;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace OpenMeteo;
 public class UrlFactory
@@ -41,130 +44,34 @@ public class UrlFactory
 
     public string GetUrlWithOptions(WeatherForecastOptions options)
     {
+        var parameters = new Dictionary<string, string>
+        {
+            [nameof(options.Latitude).ToLower()] = options.Latitude.ToString(CultureInfo.InvariantCulture),
+            [nameof(options.Longitude).ToLower()] = options.Longitude.ToString(CultureInfo.InvariantCulture),
+            [nameof(options.Temperature_Unit).ToLower()] = options.Temperature_Unit.ToString(),
+            [nameof(options.Windspeed_Unit).ToLower()] = options.Windspeed_Unit.ToString(),
+            [nameof(options.Precipitation_Unit).ToLower()] = options.Precipitation_Unit.ToString(),
+            [nameof(options.Timezone).ToLower()] = options.Timezone,
+            [nameof(options.Timeformat).ToLower()] = options.Timeformat.ToString(),
+            [nameof(options.Past_Days).ToLower()] = options.Past_Days.ToString(),
+            [nameof(options.Start_date).ToLower()] = options.Start_date,
+            [nameof(options.End_date).ToLower()] = options.End_date,
+            [nameof(options.Cell_Selection).ToLower()] = options.Cell_Selection.ToString()
+        };
+        var collections = new Dictionary<string, IEnumerable<string>>();
+        if (options.Hourly.Count >0)
+            collections[nameof(options.Hourly).ToLower()] = options.Hourly.Parameter.Select(x => x.ToString());
+        if (options.Daily.Count >0)
+            collections[nameof(options.Daily).ToLower()] = options.Daily.Parameter.Select(x => x.ToString());
+        if (options.Models.Count >0)
+            collections[nameof(options.Models).ToLower()] = options.Models.Parameter.Select(x => x.ToString());
+        if (options.Current.Count >0)
+            collections[nameof(options.Current).ToLower()] = options.Current.Parameter.Select(x => x.ToString());
+        if (options.Minutely15.Count >0)
+            collections[nameof(options.Minutely15).ToLower()] = options.Minutely15.Parameter.Select(x => x.ToString());
+
         UriBuilder uri = new(GetBaseUrl(_weatherApiUrl));
-
-        // Add the properties
-        // Begin with Latitude and Longitude since they're required
-        uri.Query = "latitude=" + options.Latitude.ToString(CultureInfo.InvariantCulture);
-        uri.Query += "&longitude=" + options.Longitude.ToString(CultureInfo.InvariantCulture);
-
-        uri.Query += "&temperature_unit=" + options.Temperature_Unit.ToString();
-        uri.Query += "&windspeed_unit=" + options.Windspeed_Unit.ToString();
-        uri.Query += "&precipitation_unit=" + options.Precipitation_Unit.ToString();
-        if (options.Timezone != string.Empty)
-            uri.Query += "&timezone=" + options.Timezone;
-
-        uri.Query += "&timeformat=" + options.Timeformat.ToString();
-
-        uri.Query += "&past_days=" + options.Past_Days;
-
-        if (options.Start_date != string.Empty)
-            uri.Query += "&start_date=" + options.Start_date;
-        if (options.End_date != string.Empty)
-            uri.Query += "&end_date=" + options.End_date;
-
-        // Now we iterate through hourly and daily
-
-        // Hourly
-        if (options.Hourly.Count > 0)
-        {
-            bool firstHourlyElement = true;
-            uri.Query += "&hourly=";
-
-            foreach (var option in options.Hourly)
-            {
-                if (firstHourlyElement)
-                {
-                    uri.Query += option.ToString();
-                    firstHourlyElement = false;
-                }
-                else
-                {
-                    uri.Query += "," + option.ToString();
-                }
-            }
-        }
-
-        // Daily
-        if (options.Daily.Count > 0)
-        {
-            bool firstDailyElement = true;
-            uri.Query += "&daily=";
-            foreach (var option in options.Daily)
-            {
-                if (firstDailyElement)
-                {
-                    uri.Query += option.ToString();
-                    firstDailyElement = false;
-                }
-                else
-                {
-                    uri.Query += "," + option.ToString();
-                }
-            }
-        }
-
-        // 0.2.0 Weather models
-        // cell_selection
-        uri.Query += "&cell_selection=" + options.Cell_Selection;
-
-        // Models
-        if (options.Models.Count > 0)
-        {
-            bool firstModelsElement = true;
-            uri.Query += "&models=";
-            foreach (var option in options.Models)
-            {
-                if (firstModelsElement)
-                {
-                    uri.Query += option.ToString();
-                    firstModelsElement = false;
-                }
-                else
-                {
-                    uri.Query += "," + option.ToString();
-                }
-            }
-        }
-
-        // new current parameter
-        if (options.Current.Count > 0)
-        {
-            bool firstCurrentElement = true;
-            uri.Query += "&current=";
-            foreach (var option in options.Current)
-            {
-                if (firstCurrentElement)
-                {
-                    uri.Query += option.ToString();
-                    firstCurrentElement = false;
-                }
-                else
-                {
-                    uri.Query += "," + option.ToString();
-                }
-            }
-        }
-
-        // new minutely_15 parameter
-        if (options.Minutely15.Count > 0)
-        {
-            bool firstMinutelyElement = true;
-            uri.Query += "&minutely_15=";
-            foreach (var option in options.Minutely15)
-            {
-                if (firstMinutelyElement)
-                {
-                    uri.Query += option.ToString();
-                    firstMinutelyElement = false;
-                }
-                else
-                {
-                    uri.Query += "," + option.ToString();
-                }
-            }
-        }
-
+        uri.Query = BuildQueryString(parameters, collections);
         SetApiKeyIfNeeded(uri);
         return uri.ToString();
     }
@@ -175,20 +82,15 @@ public class UrlFactory
     /// <returns>url+queryString</returns>
     public string GetUrlWithOptions(GeocodingOptions options)
     {
+        var parameters = new Dictionary<string, string>
+        {
+            [nameof(options.Name).ToLower()] = options.Name,
+            [nameof(options.Count).ToLower()] = options.Count >0 ? options.Count.ToString() : null,
+            [nameof(options.Format).ToLower()] = options.Format,
+            [nameof(options.Language).ToLower()] = options.Language
+        };
         UriBuilder uri = new(GetBaseUrl(_geocodeApiUrl));
-
-        // Now we check every property and set the value, if neccessary
-        uri.Query = "name=" + options.Name;
-
-        if (options.Count > 0)
-            uri.Query += "&count=" + options.Count;
-
-        if (options.Format != string.Empty)
-            uri.Query += "&format=" + options.Format;
-
-        if (options.Language != string.Empty)
-            uri.Query += "&language=" + options.Language;
-
+        uri.Query = BuildQueryString(parameters);
         SetApiKeyIfNeeded(uri);
         return uri.ToString();
     }
@@ -199,51 +101,33 @@ public class UrlFactory
     /// <returns>url+queryString</returns>
     public string GetUrlWithOptions(AirQualityOptions options)
     {
-        UriBuilder uri = new(GetBaseUrl(_airQualityApiUrl));
-
-        // Now we check every property and set the value, if neccessary
-        uri.Query += "latitude=" + options.Latitude.ToString(CultureInfo.InvariantCulture);
-        uri.Query += "&longitude=" + options.Longitude.ToString(CultureInfo.InvariantCulture);
-
-        if (options.Domains != string.Empty)
-            uri.Query += "&domains=" + options.Domains;
-
-        if (options.Timeformat != string.Empty)
-            uri.Query += "&timeformat=" + options.Timeformat;
-
-        if (options.Timezone != string.Empty)
-            uri.Query += "&timezone=" + options.Timezone;
-
-        // Finally add hourly array
-        if (options.Hourly.Count >= 0)
+        var parameters = new Dictionary<string, string>
         {
-            bool firstHourlyElement = true;
-            uri.Query += "&hourly=";
+            [nameof(options.Latitude).ToLower()] = options.Latitude.ToString(CultureInfo.InvariantCulture),
+            [nameof(options.Longitude).ToLower()] = options.Longitude.ToString(CultureInfo.InvariantCulture),
+            [nameof(options.Domains).ToLower()] = options.Domains,
+            [nameof(options.Timeformat).ToLower()] = options.Timeformat,
+            [nameof(options.Timezone).ToLower()] = options.Timezone
+        };
+        var collections = new Dictionary<string, IEnumerable<string>>();
+        if (options.Hourly.Count >0)
+            collections[nameof(options.Hourly).ToLower()] = options.Hourly.Parameter.Select(x => x.ToString());
 
-            foreach (var option in options.Hourly)
-            {
-                if (firstHourlyElement)
-                {
-                    uri.Query += option.ToString();
-                    firstHourlyElement = false;
-                }
-                else
-                {
-                    uri.Query += "," + option.ToString();
-                }
-            }
-        }
-
+        UriBuilder uri = new(GetBaseUrl(_airQualityApiUrl));
+        uri.Query = BuildQueryString(parameters, collections);
         SetApiKeyIfNeeded(uri);
         return uri.ToString();
     }
 
     public string GetUrlWithOptions(ElevationOptions options)
     {
-        UriBuilder uri = new(GetBaseUrl(_elevationApiUrl))
+        var parameters = new Dictionary<string, string>
         {
-            Query = $"latitude={options.Latitude.ToString(CultureInfo.InvariantCulture)}&longitude={options.Longitude.ToString(CultureInfo.InvariantCulture)}"
+            [nameof(options.Latitude).ToLower()] = options.Latitude.ToString(CultureInfo.InvariantCulture),
+            [nameof(options.Longitude).ToLower()] = options.Longitude.ToString(CultureInfo.InvariantCulture)
         };
+        UriBuilder uri = new(GetBaseUrl(_elevationApiUrl));
+        uri.Query = BuildQueryString(parameters);
         SetApiKeyIfNeeded(uri);
         return uri.ToString();
     }
@@ -268,5 +152,25 @@ public class UrlFactory
         }
         var prependCustomerIfHasApiKey = string.IsNullOrEmpty(_apiKey) ? string.Empty : _customerApiUrlFragment;
         return $"https://{prependCustomerIfHasApiKey}{url}";
+    }
+
+    private static string BuildQueryString(Dictionary<string, string> parameters, Dictionary<string, IEnumerable<string>> collections = null)
+    {
+        var queryParts = new List<string>();
+        foreach (var kvp in parameters)
+        {
+            if (!string.IsNullOrEmpty(kvp.Value))
+                queryParts.Add($"{Uri.EscapeDataString(kvp.Key)}={Uri.EscapeDataString(kvp.Value)}");
+        }
+        if (collections != null)
+        {
+            foreach (var kvp in collections)
+            {
+                var joined = string.Join(",", kvp.Value);
+                if (!string.IsNullOrEmpty(joined))
+                    queryParts.Add($"{Uri.EscapeDataString(kvp.Key)}={Uri.EscapeDataString(joined)}");
+            }
+        }
+        return string.Join("&", queryParts);
     }
 }
