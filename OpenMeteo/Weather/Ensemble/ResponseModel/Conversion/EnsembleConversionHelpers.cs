f878051575bbbaff9334c36dce17899c1ba012ef
colinnuk <colinnuk@users.noreply.github.com>
2025-12-15 18:32:39 -0800

    Cleanup some of the code

---
+++ OpenMeteo/Weather/Ensemble/ResponseModel/Conversion/EnsembleConversionHelpers.cs
@@ -0,0 +1,154 @@
+﻿using openmeteo_sdk;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System;
+
+namespace OpenMeteo.Weather.Ensemble.ResponseModel.Conversion;
+
+internal static class EnsembleConversionHelpers
+{
+    public static string BuildPropertyName(VariableWithValues variable)
+    {
+        var variableName = variable.Variable.ToString().ToLower();
+        var aggregation = variable.Aggregation.ToString().ToLower();
+        var altitude = variable.Altitude;
+        var pressureLevel = variable.PressureLevel;
+        var depth = variable.Depth;
+        var depthTo = variable.DepthTo;
+
+        var propertyName = variableName;
+
+        if (altitude > 0)
+        {
+            propertyName += $"{EnsembleConversionConstants.UnderscoreSeparator}{altitude}{EnsembleConversionConstants.UnitSuffixMeters}";
+        }
+
+        if (pressureLevel > 0)
+        {
+            propertyName += $"{EnsembleConversionConstants.UnderscoreSeparator}{pressureLevel}{EnsembleConversionConstants.UnitSuffixHectoPascals}";
+        }
+
+        if (depth > 0 || depthTo > 0)
+        {
+            if (depthTo > 0)
+            {
+                propertyName += $"{EnsembleConversionConstants.UnderscoreSeparator}{depth}{EnsembleConversionConstants.UnderscoreSeparator}{EnsembleConversionConstants.ToSeparator}{EnsembleConversionConstants.UnderscoreSeparator}{depthTo}{EnsembleConversionConstants.UnitSuffixCentimeters}";
+            }
+            else
+            {
+                propertyName += $"{EnsembleConversionConstants.UnderscoreSeparator}{depth}{EnsembleConversionConstants.UnitSuffixCentimeters}";
+            }
+        }
+
+        if (aggregation != EnsembleConversionConstants.AggregationNone && !string.IsNullOrEmpty(aggregation))
+        {
+            propertyName += $"{EnsembleConversionConstants.UnderscoreSeparator}{aggregation}";
+        }
+
+        return ToPascalCase(propertyName);
+    }
+
+    public static string ToPascalCase(string variableName)
+    {
+        var parts = variableName.Split(EnsembleConversionConstants.UnderscoreSeparator);
+        var result = new StringBuilder();
+        
+        for (int i = 0; i < parts.Length; i++)
+        {
+            var part = parts[i];
+            if (part.Length == 0) continue;
+            
+            result.Append(char.ToUpper(part[0]));
+            if (part.Length > 1)
+            {
+                result.Append(part.AsSpan(1));
+            }
+            
+            if (i < parts.Length - 1)
+            {
+                var nextPart = parts[i + 1];
+                if (nextPart.Length > 0 && ShouldInsertUnderscore(nextPart))
+                {
+                    result.Append(EnsembleConversionConstants.UnderscoreSeparator);
+                }
+            }
+        }
+        
+        return result.ToString();
+    }
+
+    private static bool ShouldInsertUnderscore(string nextPart)
+    {
+        return char.IsDigit(nextPart[0]) || 
+               nextPart == EnsembleConversionConstants.AggregationMean || 
+               nextPart == EnsembleConversionConstants.AggregationMin || 
+               nextPart == EnsembleConversionConstants.AggregationMax || 
+               nextPart == EnsembleConversionConstants.AggregationSum || 
+               nextPart == EnsembleConversionConstants.AggregationDominant || 
+               nextPart == EnsembleConversionConstants.AggregationHours ||
+               nextPart == EnsembleConversionConstants.ToSeparator || 
+               nextPart.EndsWith(EnsembleConversionConstants.UnitSuffixHectoPascals) || 
+               nextPart.EndsWith(EnsembleConversionConstants.UnitSuffixMeters) || 
+               nextPart.EndsWith(EnsembleConversionConstants.UnitSuffixCentimeters);
+    }
+
+    public static void AssignProperties<T>(
+        T target,
+        Dictionary<string, Dictionary<int, float?[]?>> floatMembers,
+        Dictionary<string, Dictionary<int, int?[]?>> intMembers)
+    {
+        var type = typeof(T);
+        
+        foreach (var (propertyName, members) in floatMembers)
+        {
+            var property = type.GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+            if (property != null && property.PropertyType == typeof(Dictionary<int, float?[]?>))
+            {
+                property.SetValue(target, members);
+            }
+        }
+        
+        foreach (var (propertyName, members) in intMembers)
+        {
+            var property = type.GetProperty(propertyName, BindingFlags.Public | BindingFlags.Instance | BindingFlags.IgnoreCase);
+            if (property != null && property.PropertyType == typeof(Dictionary<int, int?[]?>))
+            {
+                property.SetValue(target, members);
+            }
+        }
+    }
+
+    public static void ProcessVariables(
+        VariablesWithTime fbData,
+        Dictionary<string, Dictionary<int, float?[]?>> floatMembers,
+        Dictionary<string, Dictionary<int, int?[]?>> intMembers)
+    {
+        for (int i = 0; i < fbData.VariablesLength; i++)
+        {
+            var variable = fbData.Variables(i);
+            if (variable == null) continue;
+
+            int memberNumber = variable.Value.EnsembleMember;
+            string propertyName = BuildPropertyName(variable.Value);
+
+            var int64Values = variable.Value.GetValuesInt64Array();
+            bool isIntVariable = int64Values != null && int64Values.Length > 0;
+
+            if (isIntVariable)
+            {
+                var intValues = EnsembleMemberParser.ToNullableIntArray(variable.Value);
+                if (!intMembers.ContainsKey(propertyName))
+                    intMembers[propertyName] = [];
+                intMembers[propertyName][memberNumber] = intValues;
+            }
+            else
+            {
+                var floatValues = EnsembleMemberParser.ToNullableFloatArray(variable.Value);
+                if (!floatMembers.ContainsKey(propertyName))
+                    floatMembers[propertyName] = [];
+                floatMembers[propertyName][memberNumber] = floatValues;
+            }
+        }
+    }
+}
--- OpenMeteo/Weather/Ensemble/ResponseModel/Conversion/WeatherEnsembleDailyConversion.cs
+++ OpenMeteo/Weather/Ensemble/ResponseModel/Conversion/WeatherEnsembleDailyConversion.cs
@@ -1,4 +1,4 @@
-using openmeteo_sdk;
+﻿using openmeteo_sdk;
 using OpenMeteo.Weather.Ensemble.Options;
 using System;
 using System.Collections.Generic;
@@ -20,154 +20,12 @@ internal static class WeatherEnsembleDailyConversion
                 .Select(str => DateOnly.Parse(str)).ToArray()
         };
 
-        // Temporary dictionaries to accumulate data before assigning to properties
         var floatMembers = new Dictionary<string, Dictionary<int, float?[]?>>();
         var intMembers = new Dictionary<string, Dictionary<int, int?[]?>>();
 
-        // Convert all variables and group by ensemble member
-        for (int i = 0; i < fbDaily.Value.VariablesLength; i++)
-        {
-            var variable = fbDaily.Value.Variables(i);
-            if (variable == null) continue;
-
-            // Get the member number from the FlatBuffer field
-            int memberNumber = variable.Value.EnsembleMember;
-            
-            // Build the property name from the variable's metadata
-            string propertyName = BuildPropertyName(variable.Value);
-
-            // Determine if this is an integer or float variable
-            var int64Values = variable.Value.GetValuesInt64Array();
-            bool isIntVariable = int64Values != null && int64Values.Length > 0;
-
-            if (isIntVariable)
-            {
-                var intValues = EnsembleMemberParser.ToNullableIntArray(variable.Value);
-                if (!intMembers.ContainsKey(propertyName))
-                    intMembers[propertyName] = [];
-                intMembers[propertyName][memberNumber] = intValues;
-            }
-            else
-            {
-                var floatValues = EnsembleMemberParser.ToNullableFloatArray(variable.Value);
-                if (!floatMembers.ContainsKey(propertyName))
-                    floatMembers[propertyName] = [];
-                floatMembers[propertyName][memberNumber] = floatValues;
-            }
-        }
-
-        // Assign to properties using reflection
-        AssignProperties(daily, floatMembers, intMembers);
+        EnsembleConversionHelpers.ProcessVariables(fbDaily.Value, floatMembers, intMembers);
+        EnsembleConversionHelpers.AssignProperties(daily, floatMembers, intMembers);
 
         return daily;
-    }
-
-    private static string BuildPropertyName(VariableWithValues variable)
-    {
-        var variableName = variable.Variable.ToString().ToLower();
-        var aggregation = variable.Aggregation.ToString().ToLower();
-        var altitude = variable.Altitude;
-        var pressureLevel = variable.PressureLevel;
-        var depth = variable.Depth;
-        var depthTo = variable.DepthTo;
-
-        // Start with the base variable name
-        var propertyName = variableName;
-
-        // Add altitude if present (e.g., temperature_2m)
-        if (altitude > 0)
-        {
-            propertyName += $"_{altitude}m";
-        }
-
-        // Add pressure level if present (e.g., temperature_500hPa)
-        if (pressureLevel > 0)
-        {
-            propertyName += $"_{pressureLevel}hPa";
-        }
-
-        // Add depth range if present (e.g., soil_temperature_0_to_10cm)
-        if (depth > 0 || depthTo > 0)
-        {
-            if (depthTo > 0)
-            {
-                propertyName += $"_{depth}_to_{depthTo}cm";
-            }
-            else
-            {
-                propertyName += $"_{depth}cm";
-            }
-        }
-
-        // Add aggregation suffix if not "none" (e.g., temperature_2m_max)
-        if (aggregation != "none" && !string.IsNullOrEmpty(aggregation))
-        {
-            propertyName += $"_{aggregation}";
-        }
-
-        return ToPascalCase(propertyName);
-    }
-
-    private static string ToPascalCase(string variableName)
-    {
-        // Convert snake_case to PascalCase while preserving numeric suffixes
-        var parts = variableName.Split('_');
-        var result = new System.Text.StringBuilder();
-        
-        for (int i = 0; i < parts.Length; i++)
-        {
-            var part = parts[i];
-            if (part.Length == 0) continue;
-            
-            // Capitalize first letter
-            result.Append(char.ToUpper(part[0]));
-            if (part.Length > 1)
-            {
-                result.Append(part.Substring(1));
-            }
-            
-            // Add underscore if next part starts with a digit or is a known suffix
-            if (i < parts.Length - 1)
-            {
-                var nextPart = parts[i + 1];
-                if (nextPart.Length > 0 && (char.IsDigit(nextPart[0]) || 
-                    nextPart == "mean" || nextPart == "min" || nextPart == "max" || 
-                    nextPart == "sum" || nextPart == "dominant" || nextPart == "hours" ||
-                    nextPart == "to" || nextPart.EndsWith("hPa") || nextPart.EndsWith("m") || nextPart.EndsWith("cm")))
-                {
-                    result.Append('_');
-                }
-            }
-        }
-        
-        return result.ToString();
-    }
-
-    private static void AssignProperties(
-        EnsembleDaily daily,
-        Dictionary<string, Dictionary<int, float?[]?>> floatMembers,
-        Dictionary<string, Dictionary<int, int?[]?>> intMembers)
-    {
-        var type = typeof(EnsembleDaily);
-        
-        // Map float properties
-        foreach (var (propertyName, members) in floatMembers)
-        {
-            var property = type.GetProperty(propertyName, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.IgnoreCase);
-            if (property != null && property.PropertyType == typeof(Dictionary<int, float?[]?>))
-            {
-                property.SetValue(daily, members);
-            }
-        }
-        
-        // Map int properties
-        foreach (var (propertyName, members) in intMembers)
-        {
-            var property = type.GetProperty(propertyName, System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.IgnoreCase);
-            if (property != null && property.PropertyType == typeof(Dictionary<int, int?[]?>))
-            {
-                property.SetValue(daily, members);
-            }
-        }
     }
 }
